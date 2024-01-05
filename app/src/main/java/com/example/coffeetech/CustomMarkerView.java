6fa670ffba3f5268a3a4623dc20c84205642ce7c
nicko <nickolandermilan30@gmail.com>
2024-01-05 19:34:44 +0800

    January_5_2024_Push

--- app/src/main/java/com/example/coffeetech/CustomMarkerView.java
+++ app/src/main/java/com/example/coffeetech/CustomMarkerView.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
 
 public class CustomMarkerView extends MarkerView {
 
@@ -23,5 +24,11 @@ public class CustomMarkerView extends MarkerView {
         markerText.setText("Value: " + e.getY());
 
         super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        // Adjust the offset to move the marker view above the point
+        return new MPPointF(-(getWidth() / 2), -getHeight());
     }
 }
